ed3712b34de166afcd1154198200c386ca7a573a
Vaclav Synacek <easek@gmail.com>
2017-02-23 07:07:33 +0100

    get rid of the ocaml warnings

--- src/index.ml
+++ src/index.ml
@@ -1,8 +1,6 @@
-open Js
-
-let some_function event context callback =
+let some_function _ _ callback =
   Js.log "Ocaml in AWS Lambda";
-  let response = [%bs.obj { statusCode = 200; body = { message = "Ocaml / Buckle script output from AWS Lambda"; event = event } }] in
+  let response = [%bs.obj { statusCode = 200; body = { message = "Ocaml / Buckle script output from AWS Lambda" } }] in
   callback Js.null response
 
 class type _fParams = object
@@ -16,7 +14,7 @@ end [@bs]
 type event = _event Js.t
 
 
-let factorial_function event context callback =
+let factorial_function event _ callback =
   let factorial n =
     let rec loop i accum =
       if i > n then accum
@@ -25,5 +23,5 @@ let factorial_function event context callback =
   Js.log ("Factorial in AWS Lambda for number " ^ event##pathParameters##num);
   let n = int_of_string event##pathParameters##num in
   let result = factorial n in
-  let response = [%bs.obj { statusCode = 200; body = { message = "Factorial computation in OCaml/Bucklescript"; input = event##pathParameters##num ; output = result} }] in
+  let response = [%bs.obj { statusCode = 200; body = { message = "Factorial computation in OCaml/Bucklescript"; input = (string_of_int n) ; output = result} }] in
   callback Js.null response
